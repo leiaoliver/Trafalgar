c4604b59617c6e4196d626c95630ab0ea4c6481f
Léia Oliveira <leiaoliver388@gmail.com>
2022-01-13 11:29:28 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+/* mudar o header da página quando der scroll */
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+function changeHeaderWhenScroll() {
+  if (window.scrollY >= navHeight) {
+    // scroll é maior que a altura do header
+    header.classList.add('scroll')
+  } else {
+    // menor que a altura do header
+    header.classList.remove('scroll')
+  }
+}
+
+document.addEventListener('scroll', () => {
+  changeHeaderWhenScroll()
+})
+
+/* Testimonials carousel slider swiper */
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true,
+  breakpoints: {
+    767: {
+      slidesPerView: 1,
+      setWrapperSize: true
+    }
+  }
+})
